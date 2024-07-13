d274d593dc3b7a5de46f986a5cc491ad45d21ef8
devzeus <hieu01bdvn@gmail.com>
2024-07-13 23:59:45 +0700

    add _LayoutAdmin

--- TechShop/obj/Debug/net8.0/TechShop.AssemblyInfo.cs
+++ TechShop/obj/Debug/net8.0/TechShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be94916da4d7c1b8383f95ecb25e78779550eacb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfb29b6626770a13564572435603611b55615b01")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TechShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
