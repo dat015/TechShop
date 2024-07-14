04a313fe98895fd35ef29f108ef18153c81b2c99
devzeus <hieu01bdvn@gmail.com>
2024-07-15 00:44:27 +0700

    Add Category Controller and some actions

--- TechShop/Controllers/CategoryController.cs
+++ TechShop/Controllers/CategoryController.cs
@@ -1,12 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TechShop.Data;
+using TechShop.Models;
 
 namespace TechShop.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db; 
+        }
         public IActionResult Index()
+        {
+            List<Category> lstCat = _db.Categories.ToList();
+            return View(lstCat);
+        }
+        public IActionResult Create()
         {
             return View();
         }
+
     }
 }
