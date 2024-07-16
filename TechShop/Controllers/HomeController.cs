5f976982780b64c3c40fd1013e21d66b2a2feb72
dat015 <datnguyen151004@gmail.com>
2024-07-16 09:15:31 +0700

    update home content

--- TechShop/Controllers/HomeController.cs
+++ TechShop/Controllers/HomeController.cs
@@ -1,21 +1,35 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using TechShop.Models;
+using TechShop.Data;
 
 namespace TechShop.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
         {
-            return View();
+            List<Category> list_cate = _db.Categories.ToList();
+            List<Product> list_product = _db.Products.ToList();
+            var viewModel = new HomeViewModel(list_cate, list_product);
+
+
+            return View(viewModel);
+        }
+
+        public IActionResult ProductByCategory(int idCate)
+        {
+            List<Product> list_product = _db.Products.Where(x => x.CategoryId == idCate).OrderBy(x => x.ProductName).ToList();
+            return View(list_product);
         }
 
 
