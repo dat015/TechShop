644b5400e1a8879344d74960422bca0bbb122a49
dat015 <datnguyen151004@gmail.com>
2024-07-19 09:10:54 +0700

    update

--- TechShop/obj/Debug/net8.0/TechShop.AssemblyInfo.cs
+++ TechShop/obj/Debug/net8.0/TechShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c83a6e093ff50dd71adabaea2856f473eea186f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74b765fafa7879d5336ba19cae3d2f5810a435a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TechShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
