fec57cddaf8c27076799ac9456aac8621dc7b44c
dat015 <datnguyen151004@gmail.com>
2024-07-23 12:10:51 +0700

    fix display loyout

--- TechShop/obj/Debug/net8.0/TechShop.AssemblyInfo.cs
+++ TechShop/obj/Debug/net8.0/TechShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6554ac0130c87f61678d48af0412a3eface35726")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+405fc8262c0fbffa1d1a1bdd08615458465a7fb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TechShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
